10b59faf3d536b7c467e87c88013ba16a8c4ddcd
gonchip <136641475+gonchip@users.noreply.github.com>
2023-09-22 01:12:58 +0000

    Clock creado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,26 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClock } from "@fortawesome/free-regular-svg-icons";
+
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
+
 //create your first component
-const Home = () => {
+const Home = (props) => {
+	console.log(props)
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container mt-4 bg-dark p-0">
+			<div className="text-center d-flex justify-content-center box-color">
+				<h1 className="p-5 m-2 bg-dark text-light rounded-5 display-5 number-size"><FontAwesomeIcon icon={faClock} /></h1>
+				<h1 className="p-5 m-2 bg-dark text-light rounded-5 display-5 number-size">{props.six}</h1>
+				<h1 className="p-5 m-2 bg-dark text-light rounded-5 display-5 number-size">{props.five}</h1>
+				<h1 className="p-5 m-2 bg-dark text-light rounded-5 display-5 number-size">{props.four}</h1>
+				<h1 className="p-5 m-2 bg-dark text-light rounded-5 display-5 number-size">{props.three}</h1>
+				<h1 className="p-5 m-2 bg-dark text-light rounded-5 display-5 number-size">{props.two}</h1>
+				<h1 className="p-5 m-2 bg-dark text-light rounded-5 display-5 number-size">{props.one}</h1>
+			</div>
 		</div>
 	);
 };
